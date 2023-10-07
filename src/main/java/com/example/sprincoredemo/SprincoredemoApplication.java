8f4418e803f227130f400d21a9d2538b2b55138c
Eduard Epurica <epurica.eduard@gmail.com>
2023-10-07 19:54:34 +0300

    Initial Commit

---
+++ src/main/java/com/example/sprincoredemo/SprincoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprincoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprincoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprincoredemoApplication.class, args);
+	}
+
+}
